f9cc9df0bb64139801379fb77be360d4884e7df2
Borislav-K <bobissss84@gmail.com>
2020-05-08 17:16:25 +0300

    Add 05-b-8000.sh solution

---
+++ scripts/05-b-8000.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Напишете shell script, който получава като единствен аргумент име на потребител и за всеки негов процес изписва съобщение 
+#за съотношението на RSS към VSZ. Съобщенията да са сортирани, като процесите с най-много заета виртуална памет са най-отгоре.
+
+if [ $# -ne 1 ]
+then
+	echo "Usage: $0 <username>"
+	exit 2
+fi
+
+who | awk '{print $1}' | egrep -q "^${1}$"
+if [ $? -ne 0 ]
+then
+	echo "Error. User $1 not logged"
+	exit 1
+fi
+
+while read PID RSS VSZ
+do
+	PROPORTION="inf"
+	if [ ${VSZ} -ne 0 ]
+	then
+		#PROPORTION=$(echo "${RSS}/${VSZ}" | bc)
+		PROPORTION=$(echo "${RSS} ${VSZ}" | awk '{print $1/$2}')
+	fi
+	echo "PID: ${PID} RSS/VSZ: ${PROPORTION}"
+done < <(ps -eu "${1}" -o pid,rss,vsz | tail -n +2 | sort -nrk 3)
+
