36ab4baa41d973c4a1357f76a9bbfa0c25c2e08a
Borislav-K <bobissss84@gmail.com>
2020-06-14 15:47:42 +0300

    Refactor directory structure(2)

--- scripts/05-b-2000.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#Да се напише shell скрипт, който приканва потребителя да въведе низ
-#(име) и изпечатва "Hello, низ".
-
-read -p "Enter your name:" NAME
-
-echo "Hello, $NAME"
-exit 0
-
