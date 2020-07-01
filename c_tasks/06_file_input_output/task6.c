2e9c6474c7c34cecff4707b95ba8633225f5974c
Borislav-K <bobissss84@gmail.com>
2020-07-01 19:01:55 +0300

    Add solution for 06/task6

---
+++ c_tasks/06_file_input_output/task6.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <err.h>
+int main(int argc, char* argv[]) {
+	if(argc==1) {
+		errx(1,"%s requires at least 1 argument",argv[0]);
+	}
+	char buf[1024];
+	for(int i=1;i<argc;i++) {
+		int fd=open(argv[i],O_RDONLY);
+		if(fd==-1) {
+			warn("Cannot open file %s",argv[i]);
+			close(fd);
+			continue;
+		}
+		while(1) {
+			int readBytes=read(fd,&buf,sizeof(buf));
+			if(readBytes==-1) {
+				warn("Cannot read from file %s",argv[i]);
+				break;
+			}
+			if(readBytes==0) {
+				break; //We're done
+			}
+			write(1,&buf,readBytes);
+		}
+		close(fd);
+	}
+	exit(0);
+}
