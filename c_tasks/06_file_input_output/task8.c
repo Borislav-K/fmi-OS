c39ea4031e31de9b7777ce9949dd967331606b44
Borislav-K <bobissss84@gmail.com>
2020-07-02 20:52:13 +0300

    Add solution to 06/task8.c

--- c_tasks/06_file_input_output/task8.c
+++ c_tasks/06_file_input_output/task8.c
@@ -1,103 +1,54 @@
-/* Напишете програма, която приема точно 2 аргумента. Първият може да бъде само --min, --max или --print (вижте man 3 strcmp). Вторият аргумент е двоичен файл, в който има записани цели неотрицателни двубайтови числа (uint16_t - вижте man stdint.h). Ако първият аргумент е:
+//Koпирайте файл /etc/passwd в текущата ви работна директория и променете разделителят на копирания файл от ":", на "?"
 
---min - програмата отпечатва кое е най-малкото число в двоичния файл.
---max - програмата отпечатва кое е най-голямото число в двоичния файл.
---print - програмата отпечатва на нов ред всяко число.
-Използвайте двоичния файл binary/dump или генерирайте сами такъв (правилно подравнен).
-*/
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <err.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdint.h>
-int main(int argc, char* argv[]) {
+#include <stdbool.h>
+int main() {
 	
-	if(argc!=3) {
-		errx(1,"Expected 2 arguments, got %d",argc-1);
+	int fd1=open("/etc/passwd",O_RDONLY);
+	if(fd1==-1) {
+		err(1,"Cannot open /etc/passwd for reading");
 	}
-	if( strcmp(argv[1],"--min")!=0 && strcmp(argv[1],"--max")!=0 && strcmp(argv[1],"--print")!=0) {
-		errx(2,"Error, first argument must be --min, --max, or --print");
+
+	int fd2=open("copy_passwd.txt", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if(fd2==-1) {
+		const int olderrno=errno;
+		close(fd1);
+		errno=olderrno;
+		err(2,"Cannot open/create file copy_passwd.txt in current directory");
 	}
 	
-	struct stat st;
-	if( (stat(argv[2],&st))<0) {
-		err(3,"Cannot stat file %s",argv[2]);
-	}
-	if(st.st_size%sizeof(uint16_t)!=0) {
-		err(4,"File %s has wrong format",argv[2]);
-	}
-	if(st.st_size==0) {
-		err(4,"File %s is empty",argv[2]);
-	}
-	int fd=open(argv[2],O_RDONLY);
-	if(fd==-1) {
-		err(5,"Cannot open file %s",argv[2]);
-	}
-	if(strcmp(argv[1],"--min")==0) {
-		uint16_t min_num=INT16_MAX;
-		uint16_t buf;
-		while(1) {
-			int readBytes=read(fd,&buf,sizeof(buf));
-			if(readBytes==-1) {
-				const int olderrno=errno;
-				close(fd);
-				errno=olderrno;
-				err(6,"Cannot read from file %s",argv[2]);
-			}
-			if(readBytes==0) {
-				break; //We're done
-			}
-			if(buf<min_num) {
-				min_num=buf;
-			}
+	char buf;
+	while(true) {
+		int readBytes=read(fd1,&buf,sizeof(buf));
+		if(readBytes==-1) {
+			const int olderrno=errno;
+			close(fd1);
+			close(fd2);
+			errno=olderrno;
+			err(3,"Cannot read from /etc/passwd");
 		}
-		char output[128];
-		int output_size=sprintf(output,"Minimum number: %d\n",min_num);
-		write(1,&output,output_size);
-	} else if(strcmp(argv[1],"--max")==0) {
-		uint16_t max_num=INT16_MIN;
-		uint16_t buf;
-		while(1) {
-			int readBytes=read(fd,&buf,sizeof(buf));
-			if(readBytes==-1) {
-				const int olderrno=errno;
-				close(fd);
-				errno=olderrno;
-				err(6,"Cannot read from file %s",argv[2]);
-			}
-			if(readBytes==0) {
-				break;//We're done
-			}
-			if(buf>max_num) {
-				max_num=buf;
-			}
+		if(readBytes==0) {
+			break;//We're done
 		}
-		char output[128];
-		int output_size=sprintf(output,"Maximum number: %d\n",max_num);
-		write(1,&output,output_size);
-	} else {
-		uint16_t buf;
-		char output[128];
-		while(1) {
-			int readBytes=read(fd,&buf,sizeof(buf));
-			if(readBytes==-1) {
-				const int olderrno=errno;
-				close(fd);
-				errno=olderrno;
-				err(6,"Cannot read from file %s",argv[2]);
-			}
-			if(readBytes==0) {
-				break; //We're done
-			}
-			int output_size=sprintf(output,"%d\n",buf);
-			write(1,&output,output_size);
+		if(buf==':') {
+			buf='?';
+		}
+		int writtenBytes=write(fd2,&buf,sizeof(buf));
+		if(writtenBytes==-1) {
+			const int olderrno=errno;
+			close(fd1);
+			close(fd2);
+			errno=olderrno;
+			err(4,"Cannot write to file copy_passwd.txt");
 		}
 	}
-	close(fd);
+	close(fd1);
+	close(fd2);
 	exit(0);
 }
