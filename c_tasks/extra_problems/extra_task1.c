876eba60158968c60c771b4b5804bc8e9fada747
Borislav-K <bobissss84@gmail.com>
2020-07-05 11:24:23 +0300

    Fix extra_task1.c

--- c_tasks/extra_problems/extra_task1.c
+++ c_tasks/extra_problems/extra_task1.c
@@ -1,4 +1,3 @@
-//TODO fix this task - does not work properly
 /*
  Напишете програма, която приема като аргументи имена
 на две команди, и след това прочита редове от STDIN.
@@ -6,12 +5,19 @@
 а нечетните - на STDIN на втората. Програмата излиза,
 когато и двете команди излязат.
 */
+#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
 #include <err.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+bool revert(bool value) {
+	if(value==true) {
+		return false;
+	}
+	return true;
+}
 int main(int argc, char* argv[]) {
 
 	if(argc!=3) {
@@ -28,7 +34,7 @@ int main(int argc, char* argv[]) {
 	}
 	const pid_t pid1=fork();
 	if(pid1==-1) {
-		err(3,"Could not fork(cmd1)");
+		err(3,"Could not fork(1)");
 	}
 	if(pid1==0) {
 		//@Child1
@@ -41,13 +47,13 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	//@Parent
-	close(pipe1fd[0]);
 	const pid_t pid2=fork();
 	if(pid2==-1) {
 		err(3,"Could not fork(cmd2)");
 	}
 	if(pid2==0) {
 		//@Child2
+		close(pipe1fd[0]);
 		close(pipe1fd[1]);
 		close(pipe2fd[1]);
 		dup2(pipe2fd[0],0);
@@ -59,25 +65,43 @@ int main(int argc, char* argv[]) {
 	char buf;
 	bool even_line=true;
 	while(read(0,&buf,sizeof(buf))>0) {
+		if(even_line) {
+			int wBytes=write(pipe1fd[1],&buf,sizeof(buf));
+			if(wBytes==-1) {
+				const int olderrno=errno;
+				close(pipe1fd[1]);
+				close(pipe1fd[0]);
+				close(pipe2fd[1]);
+				close(pipe2fd[0]);
+				errno=olderrno;
+				err(5,"Could not send stdin to child(1)");
+			}
+		} else {
+			int wBytes=write(pipe2fd[1],&buf,sizeof(buf));
+			if(wBytes==-1) {
+				const int olderrno=errno;
+				close(pipe1fd[1]);
+				close(pipe1fd[0]);
+				close(pipe2fd[1]);
+				close(pipe2fd[0]);
+				errno=olderrno;
+			}
+		}
 		if(buf=='\n') {
-			even_line=!even_line;
-			//continue; --> Uncomment in order not to send the \n to the commands
-		}
-		if(even_line&&write(pipe1fd[1],&buf,sizeof(buf)!=1)) {
-			err(5,"Could not write to the pipe(1)");
-		}
-		if(!even_line&&write(pipe2fd[1],&buf,sizeof(buf)!=1)) {
-			err(5,"Could not write to the pipe(2)");
+			even_line=revert(even_line);
+			//continue; //--> Uncomment in order not to send the \n to the commands
 		}
 	}
 	close(pipe1fd[1]);
-	close(pipe1fd[2]);
+	close(pipe2fd[1]);
 	if(wait(NULL)==-1) {
 		err(6,"Could not wait for child(1)");
 	}
 	if(wait(NULL)==-1) {
 		err(6,"Could not wait for child(2)");
 	}
+	close(pipe1fd[0]);
+	close(pipe2fd[0]);
 	
 	exit(0);
 }
